579634503a3bf67a766cd273ce8fb0256ab28882
Yucheng CAO <howard.eureka@gmail.com>
2024-06-30 21:40:17 +0800

    add amdgpu

--- hosts/blade-desktop/default.nix
+++ hosts/blade-desktop/default.nix
@@ -20,6 +20,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # AMD GPU
+  boot.initrd.kernelModules = ["amdgpu"];
   networking.hostName = "blade-desktop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
