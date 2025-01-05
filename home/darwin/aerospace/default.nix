b54eec1e270ebc376eed6bd1647700c3cbe02439
Yucheng CAO <howard.eureka@gmail.com>
2025-01-05 16:09:57 +0800

    add comment and remove useless

--- home/darwin/aerospace/default.nix
+++ home/darwin/aerospace/default.nix
@@ -1,4 +1,5 @@
 {config, ...}: {
+  # aerospace is installed via homebrew
   home.file.".aerospace.toml".source =
     config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/nix-config/home/darwin/aerospace/aerospace.toml";
