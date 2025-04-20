cf912ae4f18183968c0fe504d30701bbe4aeac54
Yucheng CAO <howard.eureka@gmail.com>
2025-04-20 17:48:59 +0800

    update ssh config

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -4,10 +4,15 @@
       enable = true;
 
       extraConfig = ''
-        Host *
-          UseKeychain yes
-          AddKeysToAgent yes
-          IdentityFile ~/.ssh/id_ed25519
+      Host *
+        UseKeychain yes
+        AddKeysToAgent yes
+        IdentityFile ~/.ssh/id_ed25519
+
+      Host github.com
+        Hostname ssh.github.com
+        Port 443
+        User git
       '';
     };
   };
