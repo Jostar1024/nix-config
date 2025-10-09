a5a908dee8e0d1dfb458da581f8173795c2fb368
Yucheng Cao <yuchengcao@tubitv.com>
2025-10-09 16:42:48 +0800

    [nix] write config with different stage

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -25,26 +25,35 @@
       KERL_INSTALL_MANPAGES = "yes";
       KERL_INSTALL_HTMLDOCS = "yes";
     };
-    initContent = ''
-      # export LANG=en_US.UTF-8
-      # for doom emacs commands
-      export PATH=$PATH:~/.config/emacs/bin
 
-      # -X leaves file contents on the screen when less exits.
-      # -F makes less quit if the entire output can be displayed on one screen.
-      # -R displays ANSI color escape sequences in "raw" form.
-      # -S disables line wrapping. Side-scroll to see long lines.
-      export LESS="-SRXF"
-      export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+    initContent = let
+      zshConfigEarlyInit = lib.mkOrder 500 "zstyle ':omz:plugins:nvm' lazy yes";
+      zshConfig =
+        lib.mkOrder
+        1000
+        ''
+          # export LANG=en_US.UTF-8
+          # for doom emacs commands
+          export PATH=$PATH:~/.config/emacs/bin
 
-      source ~/.zshrc
+          # -X leaves file contents on the screen when less exits.
+          # -F makes less quit if the entire output can be displayed on one screen.
+          # -R displays ANSI color escape sequences in "raw" form.
+          # -S disables line wrapping. Side-scroll to see long lines.
+          export LESS="-SRXF"
+          export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
-      # NOTE: this requires the compinit, which is configured in oh-my-zsh's script
-      # use this to ensure that zoxide init zsh runs after the compinit
-      eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
-      . "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh"
-      . "${pkgs.asdf-vm}/share/bash-completion/completions/asdf.bash"
-    '';
+          source ~/.zshrc
+        '';
+      zshLast = lib.mkOrder 1500 ''
+        # NOTE: this requires the compinit, which is configured in oh-my-zsh's script
+        # use this to ensure that zoxide init zsh runs after the compinit
+        eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
+        . "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh"
+        . "${pkgs.asdf-vm}/share/bash-completion/completions/asdf.bash"
+      '';
+    in
+      lib.mkMerge [zshConfigEarlyInit zshConfig zshLast];
     profileExtra = ''
       eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
@@ -53,7 +62,7 @@
     autocd = true;
     oh-my-zsh = {
       enable = true;
-      plugins = ["git" "sudo"];
+      plugins = ["git" "sudo" "nvm"];
     };
   };
 
