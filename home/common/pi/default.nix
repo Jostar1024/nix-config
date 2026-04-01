85c6fe9754d17c8d77d200737833412856875ed2
Yucheng CAO <howard.eureka@gmail.com>
2026-04-01 22:53:46 +0800

    [nix] update pi

--- home/common/pi/default.nix
+++ home/common/pi/default.nix
@@ -1,5 +1,4 @@
 {...}: {
-  home.file.".pi-personal/auth.json".source     = ./auth.json;
-  home.file.".pi-personal/models.json".source   = ./models.json;
-  home.file.".pi-personal/settings.json".source = ./settings.json;
+  home.file.".pi-personal/agent/models.json".source   = ./models.json;
+  home.file.".pi-personal/agent/settings.json".source = ./settings.json;
 }
