eeb5c6561f8e5f73a96656fa2a59cad6446609a6
Yucheng CAO <howard.eureka@gmail.com>
2026-06-22 23:29:31 +0800

    pi supports glm coding plan natively, no need for this.

--- home/common/pi/default.nix
+++ home/common/pi/default.nix
@@ -1,5 +1,5 @@
-{...}: {
+{
+  ...
+}: {
   imports = [./skills.nix];
-  home.file.".pi-personal/agent/models.json".source   = ./models.json;
-  home.file.".pi-personal/agent/settings.json".source = ./settings.json;
 }
